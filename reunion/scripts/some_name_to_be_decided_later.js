5b2d3b48432ded44dee56ef44d2fa0443f9bce8b
BrianS <bschweitze@gmail.com>
2014-01-02 16:18:12 -0600

    Added some templating for adding family members

--- reunion/scripts/some_name_to_be_decided_later.js
+++ reunion/scripts/some_name_to_be_decided_later.js
@@ -6,26 +6,39 @@ $(function(){
 
 	var RegistrationCollection = Backbone.Collection.extend({
 		model: Registrant,
-		url: "\restCall"
+		url: "/restCall"
 	});
 
-	var AppView = Backbone.View.extend({
-		el: $(".wrapper"),
-		initialize: function(){
-			this.membersView = MembersView();
-		}
-	});
-	
 	var MembersView = Backbone.View.extend({
 		el: $(".members"),
 		events: {
 			"click #addmore" : "addAnotherMember"
 		},
+		initialize:function(){
+			this.addAnotherMember();
+		},
 		addAnotherMember: function(){
-			this.html("<div class='fname'> First Name <input id='fname' type='text'></div><div class='lname'>Last Name<input id='fname' type='text'></div>");
+			var view = new MemberView();
+			this.$(".memberList").append(view.render().el)
 		}
 		
 	});
 
-	var App = AppView;
+	var MemberView = Backbone.View.extend({
+		template: _.template($("#member-template").html()),
+
+		render: function(){
+			this.$el.html(this.template(this.model));
+			return this;
+		}
+	});
+	var AppView = Backbone.View.extend({
+		el: $(".wrapper"),
+		initialize: function(){
+			this.membersView = new MembersView();
+		}
+	});
+
+
+	var App = new AppView;
 });
