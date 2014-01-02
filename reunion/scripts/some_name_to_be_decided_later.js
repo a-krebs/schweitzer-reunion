b6e68b843cef1b4ca2ff872ad63914cd067c3ad8
BrianS <bschweitze@gmail.com>
2014-01-02 13:48:57 -0600

    Added wireframe for registration

--- reunion/scripts/some_name_to_be_decided_later.js
+++ reunion/scripts/some_name_to_be_decided_later.js
@@ -5,13 +5,27 @@ $(function(){
 	});
 
 	var RegistrationCollection = Backbone.Collection.extend({
-		model = Registrant,
-		url = "\restCall"
+		model: Registrant,
+		url: "\restCall"
 	});
 
 	var AppView = Backbone.View.extend({
-		el: $(".wrapper")
+		el: $(".wrapper"),
+		initialize: function(){
+			this.membersView = MembersView();
+		}
 	});
 	
+	var MembersView = Backbone.View.extend({
+		el: $(".members"),
+		events: {
+			"click #addmore" : "addAnotherMember"
+		},
+		addAnotherMember: function(){
+			this.html("<div class='fname'> First Name <input id='fname' type='text'></div><div class='lname'>Last Name<input id='fname' type='text'></div>");
+		}
+		
+	});
+
 	var App = AppView;
 });
